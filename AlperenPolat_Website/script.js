979b1f0b0bdd1453c95972f8c48bbf9ab6eb5039
Alperen POLAT <104601012+Alprenplt@users.noreply.github.com>
2023-04-04 00:26:44 +0300

    Alperen Polat Web Sitesi

---
+++ AlperenPolat_Website/script.js
@@ -0,0 +1,8 @@
+var typed = new Typed(".multiple-text", {
+    strings: [ "Son Sınıf Öğrencisiyim","Bilgisayar Mühendisiyim", "Yazılım Geliştiricisiyim"],
+    typeSpeed: 30,
+    backSpeed: 20,
+    backDelay: 1000,
+    loop: true
+
+})
